fff4569474fc098d54a698c5b1863a81dbe49731
Ricky0625 <rickywong0625@gmail.com>
2024-03-05 15:44:22 +0800

    refactor: extract hero section out and implement as parallel route

---
+++ src/app/upload-photo/@hero/page.tsx
@@ -0,0 +1,62 @@
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import CheckmarkChip from "@/components/CheckmarkChip";
+import { RoundedButton, RoundedButtonProps } from "@/components/RoundedButton";
+import DropdownMenu from "@/components/DropdownMenu";
+
+const ChipLabels = ["Not blurry", "Bright enough", "Pencil writing is clear"];
+
+interface ButtonData {
+  label: string;
+  props: RoundedButtonProps;
+}
+
+// please extend this when implementing the logic
+const Buttons: ButtonData[] = [
+  {
+    label: "convert files now",
+    props: {
+      itemType: "",
+      variant: "contained",
+    },
+  },
+  {
+    label: "view results",
+    props: {
+      itemType: "secondary",
+      variant: "outlined",
+    },
+  },
+];
+
+export default function UploadPhotoHeroSection() {
+  return (
+    <Box display="grid" justifyItems="center" rowGap={8} px={6} pt={4} pb={15} sx={{ backgroundColor: 'primary.light'}}>
+      <Box justifySelf="end" sx={{ py: 1 }}>
+        <DropdownMenu />
+      </Box>
+      <Box rowGap={4} display="grid" justifyItems="center">
+        <Box rowGap={2} display="grid">
+          <Typography variant="h5" align="center">
+            Upload your slates photo
+          </Typography>
+          <Typography variant="h4" align="center">
+            Make sure that your photos are:
+          </Typography>
+        </Box>
+        <Box display="flex" columnGap={2}>
+          {ChipLabels.map((chipLabel) => (
+            <CheckmarkChip label={chipLabel} key={chipLabel} />
+          ))}
+        </Box>
+      </Box>
+      <Box display="flex" columnGap={2.5}>
+        {Buttons.map((btn) => (
+          <RoundedButton {...btn.props} size="large" key={btn.label}>
+            {btn.label}
+          </RoundedButton>
+        ))}
+      </Box>
+    </Box>
+  );
+}
