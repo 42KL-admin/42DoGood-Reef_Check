7fde5f530bf4346fe580b1190115427016b42c91
Ricky0625 <rickywong0625@gmail.com>
2024-03-05 12:15:32 +0800

    feat: add CheckmarChip and RoundedButton component

---
+++ src/components/CheckmarkChip.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import CheckCircleRounded from "@mui/icons-material/CheckCircleRounded";
+import Chip, { ChipProps } from "@mui/material/Chip";
+import { styled } from "@mui/material/styles";
+
+const StyledChip = styled(Chip)<ChipProps>(({ theme }) => ({
+  borderRadius: "8px",
+  borderColor: "black",
+  color: "black",
+  userSelect: "none",
+}));
+
+export default function CheckmarkChip({ label }: { label: string }) {
+  return (
+    <StyledChip
+      label={label}
+      icon={<CheckCircleRounded color="primary" />}
+      variant="outlined"
+    />
+  );
+}
---
+++ src/components/RoundedButton.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import Button, { ButtonProps } from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
+
+interface RoundedButtonProps extends ButtonProps {
+  itemType: string;
+  variant: "text" | "outlined" | "contained";
+}
+
+const RoundedButton = styled(Button)<RoundedButtonProps>(
+  ({ theme, itemType, variant }) => ({
+    boxShadow: "none",
+    borderRadius: "20px",
+    fontSize: "16px",
+    fontWeight: 400,
+    px: 6,
+    textTransform: "capitalize",
+    ...(itemType === "secondary"
+      ? { color: "black", borderColor: "black" }
+      : {}),
+    variant: variant || "text",
+  })
+);
+
+export { RoundedButton }; 
+export type { RoundedButtonProps };
+
