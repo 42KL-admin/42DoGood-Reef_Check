10e2b1977cf9b14a17644c3bb28c280e98b3d15b
Ricky0625 <rickywong0625@gmail.com>
2024-03-06 20:28:19 +0800

    feat(useRowControls): add & remove row function

--- src/hooks/upload/useRowControls.ts
+++ src/hooks/upload/useRowControls.ts
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Row } from "./types";
+import { Row, SlateType } from "./types";
 
 const useRowControls = () => {
   // default is just one row
@@ -35,6 +35,60 @@ const useRowControls = () => {
     setRows(updatedRows);
   };
 
+  // handle upload file
+  const setSlateFile =
+    (index: number, type: SlateType) =>
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const selectedFile = event.target.files && event.target.files[0];
+
+      if (!selectedFile) return;
+
+      setRows((prevRows) => {
+        const updatedRows = [...prevRows];
+        const targetRow = { ...updatedRows[index] };
+        const targetSlateKey =
+          type === "substrate" ? "substrate" : "fishInverts";
+
+        if (targetSlateKey in targetRow) {
+          // deep copy the target Slate to ensure immutability
+          const targetSlate = { ...targetRow[targetSlateKey] };
+          targetSlate.file = selectedFile;
+
+          // update the targetRow with the modified slate
+          const updatedRow = { ...targetRow, [targetSlateKey]: targetSlate };
+          updatedRows[index] = updatedRow;
+
+          return updatedRows;
+        }
+
+        // sanity check: if slate type is not recognized, return the previous slate
+        return prevRows;
+      });
+    };
+
+  const unsetSlateFile = (index: number, type: SlateType) => {
+    setRows((prevRows) => {
+      const updatedRows = [...prevRows];
+      const targetRow = { ...updatedRows[index] };
+      const targetSlateKey = type === "substrate" ? "substrate" : "fishInverts";
+
+      if (targetSlateKey in targetRow) {
+        // deep copy the target Slate to ensure immutability
+        const targetSlate = { ...targetRow[targetSlateKey] };
+        targetSlate.file = null;
+
+        // update the targetRow with the modified state
+        const updatedRow = { ...targetRow, [targetSlateKey]: targetSlate };
+        updatedRows[index] = updatedRow;
+
+        return updatedRows;
+      }
+
+      // sanity check: if slate type is not recognized, return the previous slate
+      return prevRows;
+    });
+  };
+
   if (rows.length === 0) {
     setRows([createRow()]);
   }
@@ -44,6 +98,8 @@ const useRowControls = () => {
     rows,
     addRow,
     removeRow,
+    setSlateFile,
+    unsetSlateFile
   };
 };
 
