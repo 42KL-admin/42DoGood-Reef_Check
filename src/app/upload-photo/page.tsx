4df9e4563fb00fc8b3000f06c3bc1185a0921b6f
Ricky0625 <rickywong0625@gmail.com>
2024-03-06 13:14:42 +0800

    feat: layout for upload photo section done

--- src/app/upload-photo/page.tsx
+++ src/app/upload-photo/page.tsx
@@ -1,7 +1,7 @@
-import InputFileUpload from "@/components/FileActionButton";
 import FileRowComponent from "@/components/FileRowComponent";
-import { Delete } from "@mui/icons-material";
+import Delete from "@mui/icons-material/Delete";
 import Add from "@mui/icons-material/Add";
+import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 
@@ -23,13 +23,27 @@ export default function UploadPhotoSection() {
 
   return (
     <Box pt={12}>
-      <Container maxWidth="xl" disableGutters>
-        <Box display="grid" rowGap={2.5}>
-          <FileRowComponent />
-          <FileRowComponent />
+      <Container maxWidth="xl" disableGutters sx={{ display: "grid" }}>
+        <Box display="grid" rowGap={2.5} maxHeight={400} overflow="scroll">
           <FileRowComponent />
           <FileRowComponent />
         </Box>
+        <Button
+          startIcon={<Add />}
+          variant="text"
+          size="large"
+          sx={{
+            textTransform: "unset",
+            color: "black",
+            fontWeight: 400,
+            fontSize: 20,
+            margin: "0 auto",
+            mt: 9,
+            width: "fit-content",
+          }}
+        >
+          Add more files
+        </Button>
       </Container>
     </Box>
   );
