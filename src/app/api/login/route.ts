f1a752cc1f3eefc649597b1a6737a58dd0f4c97c
PandieYay <babyedison@gmail.com>
2024-03-11 22:18:50 +0800

    Backend setup with mongoDB

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,9 +1,19 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import clientPromise from "../../../../lib/mongodb";
 
-export async function POST(request: Request) {
-    let data = await request.json();
-    if (data.email === "admin@gmail.com")
+export async function POST(request: NextRequest) {
+    try {
+        const client = await clientPromise;
+        const db = client.db("42reef-check");
+        let data = await request.json();
+
+        const emailExist = await db
+            .collection("users")
+            .countDocuments({ email: data.email });
+        if (!emailExist)
+            return NextResponse.json({ error: "Invalid email" }, { status: 400 });
         return NextResponse.redirect(new URL('/upload', request.url));
-    else
-        return NextResponse.json({ error: "Invalid email" });
+    } catch (e) {
+        console.error(e);
+    }
 }
