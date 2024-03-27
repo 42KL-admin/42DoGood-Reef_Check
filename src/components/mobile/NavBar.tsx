4dc87a55402501bb4426a3f932fa1885205fd589
Ricky0625 <rickywong0625@gmail.com>
2024-03-27 12:17:21 +0800

    feat: mobile nav bar add drawer

--- src/components/mobile/NavBar.tsx
+++ src/components/mobile/NavBar.tsx
@@ -1,53 +1,140 @@
-import { Menu } from "@mui/icons-material";
-import {
-  AppBar,
-  Box,
-  IconButton,
-  Toolbar,
-  Typography,
-} from "@mui/material";
+import { Close, Height, Menu } from "@mui/icons-material";
+import Drawer from "@mui/material/Drawer";
+import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
+import { Fragment, useState } from "react";
+import Link from "next/link";
 
 export default function NavBar() {
+  const [open, setOpen] = useState<boolean>(false);
+
+  const toggleDrawer = (newOpen: boolean) => () => {
+    setOpen(newOpen);
+  };
+
   return (
-    <Box
-      sx={{
-        flexGrow: 1,
-        backgroundColor: "white",
-        display: { sm: "block", md: "none" },
-        position: "fixed",
-        top: 0,
-        zIndex: 999,
-        width: "100%",
-      }}
-    >
-      <AppBar
-        sx={{ backgroundColor: "white", padding: "14px 35px" }}
-        elevation={0}
+    <Fragment>
+      <Box
+        sx={{
+          flexGrow: 1,
+          backgroundColor: "white",
+          display: { sm: "block", md: "none" },
+          position: "fixed",
+          top: 0,
+          zIndex: 999,
+          width: "100%",
+        }}
       >
-        <Toolbar sx={{ position: "relative" }}>
-          <Typography
-            variant="h6"
-            color="black"
-            align="center"
-            width="100%"
-            // sx={{ flex: 1, display: "flex", justifyContent: "center" }}
+        <AppBar
+          sx={{ backgroundColor: "white", padding: "14px 35px" }}
+          elevation={0}
+        >
+          <Toolbar sx={{ position: "relative" }}>
+            <Typography variant="h6" color="black" align="center" width="100%">
+              Upload
+            </Typography>
+            <IconButton
+              sx={{
+                color: "black",
+                position: "absolute",
+                right: 0,
+                top: "50%",
+                transform: "translateY(-50%)",
+              }}
+              size="large"
+              onClick={toggleDrawer(true)}
+            >
+              <Menu />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Drawer
+        open={open}
+        onClose={toggleDrawer(false)}
+        anchor="top"
+        sx={{
+          backgroundColor: "primary",
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            boxSizing: "border-box",
+            height: "100vh",
+            width: "100%",
+            backgroundColor: "#006878",
+            borderBottomLeftRadius: "30px",
+            borderBottomRightRadius: "30px",
+          },
+        }}
+      >
+        <Box
+          display={"flex"}
+          flexDirection="column"
+          height={"100%"}
+          pt={10}
+          pb={10}
+        >
+          <Box
+            sx={{ flexGrow: 1 }}
+            display={"flex"}
+            flexDirection={"column"}
+            justifyContent={"center"}
+            alignItems={"center"}
           >
-            Upload
-          </Typography>
-          <IconButton
-            sx={{
-              color: "black",
-              position: "absolute",
-              right: 0,
-              top: "50%",
-              transform: "translateY(-50%)",
-            }}
-            size="large"
+            <Link
+              href={"/upload"}
+              style={{ textDecoration: "none", color: "white" }}
+              onClick={toggleDrawer(false)}
+            >
+              <Typography py={4} letterSpacing={2} align="center">
+                Convert Slates
+              </Typography>
+            </Link>
+            <Link
+              href={"/results"}
+              style={{ textDecoration: "none", color: "white" }}
+              onClick={toggleDrawer(false)}
+            >
+              <Typography py={4} letterSpacing={2} align="center">
+                View My Slates
+              </Typography>
+            </Link>
+            <Link
+              href={"/upload"}
+              style={{ textDecoration: "none", color: "white" }}
+              onClick={toggleDrawer(false)}
+            >
+              <Typography py={4} letterSpacing={2} align="center">
+                Sign Out
+              </Typography>
+            </Link>
+          </Box>
+          <Box
+            mt={"auto"}
+            bottom={0}
+            display={"flex"}
+            flexDirection={"column"}
+            rowGap={8}
+            alignItems={"center"}
           >
-            <Menu />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-    </Box>
+            <Typography
+              align="center"
+              fontSize={"16px"}
+              color={"white"}
+              fontWeight={400}
+              letterSpacing={1}
+            >
+              Signed in as:
+              <br />
+              user@email.com
+            </Typography>
+            <IconButton
+              sx={{ width: "fit-content", color: "white" }}
+              onClick={toggleDrawer(false)}
+            >
+              <Close />
+            </IconButton>
+          </Box>
+        </Box>
+      </Drawer>
+    </Fragment>
   );
 }
