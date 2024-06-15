853b94153834705669632c459f8938944f03583d
Ricky0625 <rickywong0625@gmail.com>
2024-06-15 15:13:39 +0800

    feat: uploadSlateApi wrapper function

---
+++ src/services/uploadSlateApi.ts
@@ -0,0 +1,18 @@
+import { apiCall } from "@/utils/apiCall";
+
+const uploadSlateApiUrl = `api/uploadImage`;
+
+export async function uploadSlatesToBlob(files: File[]) {
+    const formData = new FormData();
+    files.forEach(file => {
+        formData.append("files", file);
+    });
+
+    try {
+        const response = await apiCall(uploadSlateApiUrl, "POST", formData, true);
+        return response;
+    } catch (e: any) {
+        console.log("error uploading slates", e.message);
+        throw new Error("uploadSlatesToBlob error", e.message);
+    }
+}
