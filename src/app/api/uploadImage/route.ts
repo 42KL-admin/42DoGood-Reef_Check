319012813c15561a97942acdc54f5681963a57e2
Thong Zi Qi <ziqi.thong@gmail.com>
2024-06-22 00:31:47 +0800

    [UPDATE] add upload status and history in UploadImages

--- src/app/api/uploadImage/route.ts
+++ src/app/api/uploadImage/route.ts
@@ -3,13 +3,26 @@ import { BlobServiceClient } from '@azure/storage-blob';
 import { Readable } from 'stream';
 import { generateResponse } from '../../../utils/response';
 import { UploadFilesRequest, File } from '../../../interfaces/api';
-import { SAS_COOKIE_NAME, isSasTokenExpired } from '../utils';
+import { SAS_COOKIE_NAME, USER_COOKIE, isSasTokenExpired } from '../utils';
 import { cookies } from 'next/headers';
+import mongoose from 'mongoose';
+import Users from '@/models/Users';
+import Uploads from '@/models/Uploads';
 
 const accountName = process.env.AZURE_STORAGE_ACCOUNT_NAME || 'default-account-name';
 const containerName = process.env.AZURE_STORAGE_CONTAINER_NAME || 'default-container-name';
 
-// Function to parse form data
+async function updateFileStatus(uploadId: mongoose.Types.ObjectId, status: string, description = '') {
+  const upload = await Uploads.findById(uploadId);
+  if (upload) {
+    upload.status = status;
+    upload.status_description = description;
+    await upload.save();
+  } else {
+    console.error(`Upload with ID ${uploadId} not found.`);
+  }
+}
+
 async function parseFormData(request: NextRequest): Promise<UploadFilesRequest> {
   const formData = await request.formData();
   const files: File[] = [];
@@ -28,11 +41,10 @@ async function parseFormData(request: NextRequest): Promise<UploadFilesRequest> 
   return { files };
 }
 
-// Function to upload files to Azure Blob Storage
-async function uploadFiles(sasToken: string, files: File[]): Promise<void> {
+async function uploadFiles(sasToken: string, files: File[], userEmail: string): Promise<void> {
   const url = `https://${accountName}.blob.core.windows.net/${containerName}?${sasToken}`;
   const blobServiceClient = new BlobServiceClient(url);
-  const containerClient = blobServiceClient.getContainerClient(containerName);
+  const containerClient = blobServiceClient.getContainerClient(userEmail);
 
   for (const { filename, fileBuffer, fileType } of files) {
     const blockBlobClient = containerClient.getBlockBlobClient(filename);
@@ -44,26 +56,72 @@ async function uploadFiles(sasToken: string, files: File[]): Promise<void> {
   }
 }
 
-// Main POST handler
+async function getUserEmailFromCookies(): Promise<string> {
+    const userCookie = cookies().get(USER_COOKIE);
+    if (!userCookie) throw new Error('User cookie missing');
+
+    const userEmail = JSON.parse(userCookie.value).email;
+    if (!userEmail) throw new Error('Email doesn\'t exist');
+    return userEmail;
+}
+
+// Function to get the user ID and email from cookies
+async function getUserIdFromCookies(): Promise<{ userId: mongoose.Types.ObjectId, userEmail: string }> {
+    const userEmail = await getUserEmailFromCookies();
+    const user = await Users.findOne({ email: userEmail });
+    if (!user) throw new Error('User not found');
+
+    return { userId: user._id, userEmail };
+}
+
+async function handleFileUpload(file: File, userId: mongoose.Types.ObjectId, sasToken: string, userEmail: string) {
+  const uploadDoc = {
+    user_id: userId,
+    directory: userEmail,
+    file_name: file.filename,
+    status: 'Uploaded',
+    status_description: '',
+    timestamp: new Date()
+  };
+
+  const result = await Uploads.create(uploadDoc);
+  const uploadId = result._id;
+
+  try {
+    await uploadFiles(sasToken, [file], userEmail);
+    await updateFileStatus(uploadId, 'Processing');
+
+    setTimeout(async () => {
+      const processingResult = true; // Placeholder for actual processing logic
+      if (processingResult) {
+        await updateFileStatus(uploadId, 'Successful');
+      } else {
+        await updateFileStatus(uploadId, 'Failed', 'Processing error details');
+      }
+    }, 3000);
+  } catch (e: any) {
+    await updateFileStatus(uploadId, 'Failed', e.message);
+  }
+}
+
 export async function POST(request: NextRequest): Promise<Response> {
   try {
     const { files } = await parseFormData(request);
     const sasToken = cookies().get(SAS_COOKIE_NAME);
+    if (!sasToken) return generateResponse({ error: 'SAS token missing' }, 400);
+    if (isSasTokenExpired()) return generateResponse({ error: 'SAS token has expired' }, 400);
+    if (files.length === 0) return generateResponse({ error: 'Files missing' }, 400);
 
-    if (!sasToken)
-      return generateResponse({ error: 'SAS token missing' }, 400);
+    const { userId, userEmail  }= await getUserIdFromCookies();
 
-    if (isSasTokenExpired())
-      return generateResponse({ error: 'SAS token has expired' }, 400);
-
-    if (files.length === 0)
-      return generateResponse({ error: 'Files missing' }, 400);
-
-    await uploadFiles(sasToken.value, files);
+    console.log('userEmail = ', userEmail);
+    
+    const uploadPromises = files.map(file => handleFileUpload(file, userId, sasToken.value, userEmail));
+    await Promise.all(uploadPromises);
 
     return generateResponse({ message: 'Files uploaded successfully' }, 200);
   } catch (e: any) {
-    console.error(e);
+    console.error(`Internal server error: ${e.message}`);
     return generateResponse({ message: 'Internal server error', error: e.message }, 500);
   }
 }
