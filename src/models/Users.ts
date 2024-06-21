3dc49c31bbdf15cb39822f40b2591835fde75f4c
Thong Zi Qi <ziqi.thong@gmail.com>
2024-06-22 00:35:05 +0800

    [UPDATE] connect Users model to db(42reef-check)

--- src/models/Users.ts
+++ src/models/Users.ts
@@ -1,12 +1,23 @@
 import mongoose, { Document, Schema, Model } from 'mongoose';
 
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const mongoURI = process.env.MONGODB_URI;
+
+mongoose.connect(mongoURI!).then(() => {
+}).catch((error) => {
+    console.error("MongoDB connection error in Users model:", error);
+});
 export interface IUsers extends Document {
+  _id   : mongoose.Schema.Types.ObjectId;
   email	: string;
   role	: string;
 }
 
 const UsersSchema: Schema = new mongoose.Schema({
-  email	: { type: String, required: true },
+  email	: { type: String, required: true, unique: true},
   role	: { type: String, enum: ['admin', 'user'], required: true },
 });
 
