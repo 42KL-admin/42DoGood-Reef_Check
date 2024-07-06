6e1b7eac787905dc4e5d54cef6e2cf984f387bf0
Lok Eu Lee <lokeulee@gmail.com>
2024-07-06 17:03:06 +0800

    [ExcelHandsontable] - refactor : apiCall now able to return blob response and commented out credentials 'include' to bypass CORS policy

--- src/utils/apiCall.ts
+++ src/utils/apiCall.ts
@@ -1,18 +1,36 @@
 type HTTPMethod = 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH';
 
-export async function apiCall(url: string, method: HTTPMethod, body?: any, isFormData: boolean = false) {
+export async function apiCall(
+  url: string,
+  method: HTTPMethod,
+  body?: any,
+  isCustomedHeader?: any,
+  isFormData: boolean = false,
+) {
+  const headers = isCustomedHeader ? isCustomedHeader : {};
+
   const options: RequestInit = {
     method,
-    headers: isFormData ? {} : { 'Content-Type': 'application/json' },
-    credentials: 'include',
+    headers:
+      isFormData && isCustomedHeader
+        ? headers
+        : { 'Content-Type': 'application/json' },
+    // credentials: 'include',
     body: isFormData ? body : JSON.stringify(body),
   };
 
   const response = await fetch(url, options);
 
   if (!response.ok) {
-    throw new Error(`API request failed with status ${response.status}, url: ${url}`);
+    throw new Error(
+      `API request failed with status ${response.status}, url: ${url}`,
+    );
   }
 
-  return response.json();
+  const contentType = response.headers.get('Content-Type');
+  if (contentType && contentType.includes('application/json')) {
+    return response.json();
+  } else {
+    return response.blob(); // Return blob data directly
+  }
 }
