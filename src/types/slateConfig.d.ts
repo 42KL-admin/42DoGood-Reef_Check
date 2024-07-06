e7a9ec2dfd4641a9253643abb256dde27aeafb88
Lok Eu Lee <lokeulee@gmail.com>
2024-07-06 17:06:41 +0800

    [ExcelHandsontable] - feat: Added slateConfig constructor

---
+++ src/types/slateConfig.d.ts
@@ -0,0 +1,9 @@
+declare namespace SlateConfig {
+  interface SlateConfig {
+    rowNumberFrom: number;
+    rowNumberTo: number;
+    numberOfColumns: number;
+    ignoredRows: number[];
+    ignoredCols: number[];
+  }
+}
