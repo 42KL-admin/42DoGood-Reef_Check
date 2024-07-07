63708c0da9d3afbac1200d6846b703c42419c316
Lok Eu Lee <lokeulee@gmail.com>
2024-07-07 19:22:46 +0800

    [ExcelHandsontable] - fix : Revert changes to ApiCall as certain api doesn't work anymore from what I have changed

--- src/utils/apiCall.ts
+++ src/utils/apiCall.ts
@@ -1,36 +1,18 @@
 type HTTPMethod = 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH';
 
-export async function apiCall(
-  url: string,
-  method: HTTPMethod,
-  body?: any,
-  isCustomedHeader?: any,
-  isFormData: boolean = false,
-) {
-  const headers = isCustomedHeader ? isCustomedHeader : {};
-
+export async function apiCall(url: string, method: HTTPMethod, body?: any, isFormData: boolean = false) {
   const options: RequestInit = {
     method,
-    headers:
-      isFormData && isCustomedHeader
-        ? headers
-        : { 'Content-Type': 'application/json' },
-    // credentials: 'include',
+    headers: isFormData ? {} : { 'Content-Type': 'application/json' },
+    credentials: 'include',
     body: isFormData ? body : JSON.stringify(body),
   };
 
   const response = await fetch(url, options);
 
   if (!response.ok) {
-    throw new Error(
-      `API request failed with status ${response.status}, url: ${url}`,
-    );
+    throw new Error(`API request failed with status ${response.status}, url: ${url}`);
   }
 
-  const contentType = response.headers.get('Content-Type');
-  if (contentType && contentType.includes('application/json')) {
-    return response.json();
-  } else {
-    return response.blob(); // Return blob data directly
-  }
+  return response.json();
 }
